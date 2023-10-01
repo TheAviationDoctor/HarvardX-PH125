e2b45e0afdfce0a509fe8de11ca7e1d869614d6b
Thomas D. Pellegrin <47185363+TheAviationDoctor@users.noreply.github.com>
2023-10-01 20:11:36 +0800

    Finished data exploration

--- movielens.R
+++ movielens.R
@@ -17,11 +17,22 @@
 
 # Install the required libraries if they are missing
 repo <- "http://cran.us.r-project.org"
-if(!require(tidyverse)) { install.packages("tidyverse", repos = repo) }
-if(!require(caret))     { install.packages("caret",     repos = repo) }
+
+# Define a function to check and install packages if not available
+install_if_not_available <- function(package_name, repo = repo) {
+  if (!requireNamespace(package_name, quietly = TRUE)) {
+    install.packages(package_name, repos = repo, dependencies = TRUE)
+  }
+}
+
+# Call the function for the required packages
+install_if_not_available("caret")
+install_if_not_available("markdown")
+install_if_not_available("tidyverse")
 
 # Load the required libraries
 library(caret)
+library(markdown)
 library(tidyverse)
 
 # Set a 2-minute timeout for attempting to download datasets
@@ -34,7 +45,11 @@ start_time <- Sys.time()
 cat("\014")
 
 # ==============================================================================
-# 1 Create the edx dataset if necessary (adapted from course-provided code)
+# 1 Prepare the data
+# ==============================================================================
+
+# ==============================================================================
+# 1.1 Create the edx dataset if necessary (adapted from course-provided code)
 # ==============================================================================
 
 # Run only if the edx dataset is not yet loaded into the R environment
@@ -114,150 +129,301 @@ if(!exists("edx")) {
 }
 
 # ==============================================================================
+# 1.2 Transform (a copy of) the data to reveal possible predictors
+# ==============================================================================
+
+# Year of the movie and of the rating, and their distance, might be predictors,
+# so extract them, remove year from titles, & remove no longer needed timestamps
+if(!exists("df")) {
+  df <- edx %>%
+    mutate(
+      year_movie  = as.integer(str_sub(title, start = -5L, end = -2L)),
+      year_rating = as.integer(as.POSIXlt(timestamp, origin = "1970-01-01")$year + 1900L),
+      year_dist   = as.integer(abs(year_movie - year_rating)),
+      title       = str_sub(title, end = -8L),
+      timestamp   = NULL
+    )
+}
+
+# ==============================================================================
 # 2 Exploratory analysis to identify what might be useful predictors of ratings
 # ==============================================================================
 
 # ==============================================================================
-# 2.1 Transform the data
+# 2.1 Initial data exploration
 # ==============================================================================
 
-# Display first 5 rows before transformations
-head(edx, 10L)
-
-# Describe the structure of the data
+# Describe the structure of the original dataset
+# 9,000,055 observations of 6 variables
 str(edx)
 
-# Copy the edx dataset to a df dataset so we can transform the data at will.
-# Year of the movie and of the rating, and their distance, might be predictors,
-# so extract them, remove year from titles, & remove no longer needed timestamps
-df <- edx %>%
-  mutate(
-    year_movie  = as.integer(str_sub(title, start = -5L, end = -2L)),
-    year_rating = as.integer(as.POSIXlt(timestamp, origin = "1970-01-01")$year + 1900L),
-    year_dist   = as.integer(abs(year_movie - year_rating)),
-    title       = str_sub(title, end = -8L),
-    timestamp   = NULL
-  )
+# Describe the structure of the transformed dataset
+# 9,000,055 observations of 8 variables
+str(df)
 
-# ==============================================================================
-# 2.1 Number of review per movie
-# ==============================================================================
+# Number of unique movies
+# 10,677
+n_distinct(df$movieId)
 
-# Ratings may be skewed if there's a very low number of them for a given movie
-# So let's determine the number of reviews per movie
-df_reviews <- df %>%
-  count(title) %>%
-  arrange(desc(n))
+# Number of unique reviewers
+# 69,878
+n_distinct(df$userId)
 
-# Display the statistics
-summary(df_reviews)
+# Mean number of ratings per movie
+# 842.9386
+nrow(df) / n_distinct(df$movieId)
 
-# Top ten movies by number of reviews
-df_reviews %>% head(10L)
-
-# Bottom ten movies by number of reviews
-df_reviews %>% tail(10L)
-
-# ==============================================================================
-# 2.2 Ratings per movie
-# ==============================================================================
-
-# Top ten movies by rating
-# We only consider movies above the 25th percentile of ratings count
-
-quantile(df_reviews$n, .25)
-
-df %>%
-  group_by(title) %>%
-  summarize(mean_rating = mean(rating)) %>%
-  arrange(desc(mean_rating)) %>%
-  head(10L)
-
-
-
-
+# Mean number of ratings per reviewer
+# 128.7967
+nrow(df) / n_distinct(df$userId)
 
 # List unique genres
+# 19 genres + one blank, drama is the most frequent
 df %>%
   distinct(title, .keep_all = TRUE) %>%
   separate_rows(genres, sep = "\\|") %>%
-  count(genres)
-  # distinct(genres)
-  # group_by(genres) %>%
-  # summarize(
-  #   # genres = as.factor(genres),
-  #   count  = n()
-  # )
-genres
-
-# Display first 5 rows after transformations
-# head(df, 5L)
+  count(genres, sort = TRUE)
 
 # ==============================================================================
-# 3 Visualize relationships in the data
+# 2.2 Overall distribution of ratings
 # ==============================================================================
 
-# # Ratings by year of movie release
-# df %>%
-#   group_by(year_movie) %>%
-#   summarize(
-#     mean   = mean(rating),
-#     median = median(rating),
-#     low    = quantile(rating, .25), # 25th percentile of the ratings for that year
-#     high   = quantile(rating, .75)  # 75th percentile of the ratings for that year
-#   ) %>%
-#   ggplot(aes(x = year_movie)) +
-#     geom_ribbon(
-#       mapping = aes(y = mean, ymin = low, ymax = high),
-#       fill    = "grey70"
-#     ) +
-#     geom_line(mapping = aes(y = mean)) +
-#     geom_point(mapping = aes(y = median)) +
-#     theme_light() +
-#     xlab("Year of release") +
-#     ylab("Ratings")
+# Ratings distribution table
+# Most frequent is 4.0 (28.8% of all ratings), followed by 3.0 (23.6%)
+# Least frequent is 0.5 (0.9% of all ratings), followed by 1.5 (1.18%)
+# There is no rating of zero
+df %>%
+  group_by(rating) %>%
+  summarize(n_ratings = n(), n_per = n() / nrow(df) * 100L)
 
-# # Ratings by year of rating
-# df %>%
-#   group_by(year_rating) %>%
-#   summarize(
-#     mean   = mean(rating),
-#     median = median(rating),
-#     low    = quantile(rating, .25), # 25th percentile of the ratings for that year
-#     high   = quantile(rating, .75)  # 75th percentile of the ratings for that year
-#   ) %>%
-#   ggplot(aes(x = year_rating)) +
-#   geom_ribbon(
-#     mapping = aes(y = mean, ymin = low, ymax = high),
-#     fill    = "grey70"
-#   ) +
-#   geom_line(mapping = aes(y = mean)) +
-#   geom_point(mapping = aes(y = median)) +
-#   theme_light() +
-#   xlab("Year of rating") +
-#   ylab("Ratings")
+# Ratings distribution histogram
+df %>%
+  ggplot(mapping = aes(x = rating)) +
+  geom_histogram() +
+  theme_light()
 
-# # Ratings by years of distance between movie release and rating
-# df %>%
-#   group_by(year_dist) %>%
-#   summarize(
-#     mean   = mean(rating),
-#     median = median(rating),
-#     low    = quantile(rating, .25), # 25th percentile of the ratings for that year
-#     high   = quantile(rating, .75)  # 75th percentile of the ratings for that year
-#   ) %>%
-#   ggplot(aes(x = year_dist)) +
-#   geom_ribbon(
-#     mapping = aes(y = mean, ymin = low, ymax = high),
-#     fill    = "grey70"
-#   ) +
-#   geom_line(mapping = aes(y = mean)) +
-#   geom_point(mapping = aes(y = median)) +
-#   theme_light() +
-#   xlab("Years of distance between movie release and rating") +
-#   ylab("Ratings")
+# ==============================================================================
+# 2.3 Number of ratings per movie
+# ==============================================================================
+
+# Ratings per movie
+# Min is 1 rating per movie, max is 31,362, mean is 842.9, median is 122
+df %>%
+  count(movieId, name = "n_ratings") %>%
+  select(n_ratings) %>%
+  summary()
+
+# Mode (most frequent number of ratings per movie)
+# The most frequent number is 4 ratings per movie (154 movies have that)
+df %>%
+  count(movieId, name = "n_ratings") %>%
+  count(n_ratings, name = "frequency", sort = TRUE) %>%
+  head(1L)
+
+# Most rated movies (top n)
+# Pulp Fiction followed by Forrest Gump are the movies with the most ratings
+df %>%
+  count(movieId, title, name = "n_ratings", sort = TRUE) %>%
+  head(10L)
+
+# Movies with only one rating
+# 126 of them, too many to list
+df %>%
+  count(movieId, title) %>%
+  filter(n == 1) %>%
+  nrow()
+
+# Movies in the bottom 25th percentile in number of ratings
+# 2,688 movies with 30 ratings or less
+df %>%
+  count(movieId) %>%
+  filter(n <= quantile(n, .25)) %>%
+  nrow()
+
+# Frequency plot of ratings per movie
+# We find a strong relationship: movies with few ratings are far more present
+df %>%
+  count(movieId, name = "n_ratings") %>%
+  count(n_ratings, name = "frequency") %>%
+  ggplot(mapping = aes(x = n_ratings, y = frequency)) +
+  geom_point() +
+  scale_x_log10() +
+  theme_light()
+
+# Maybe do a Pareto here?
+
+# ==============================================================================
+# 2.4 Rating score per movie
+# ==============================================================================
+
+# Highest-rated movies (top n)
+# We find that highest-rated movies also have very few (1–4) ratings each
+df %>%
+  group_by(movieId, title) %>%
+  summarize(mean_rating = mean(rating), n_rating = n()) %>%
+  arrange(desc(mean_rating)) %>%
+  head(10L)
+
+# Same, but now excluding the 25th bottom percentile in # of ratings
+# This returns a totally different set (zero overlap)
+df %>%
+  group_by(movieId, title) %>%
+  summarize(mean_rating = mean(rating), n_rating = n()) %>%
+  arrange(desc(mean_rating)) %>%
+  filter(n_rating > 30L) %>%
+  head(10L)
+
+# Lowest-rated movies (bottom n)
+# We find that lowest-rated movies also have very few (1–199) ratings each
+df %>%
+  group_by(movieId, title) %>%
+  summarize(mean_rating = mean(rating), n_rating = n()) %>%
+  arrange(desc(mean_rating)) %>%
+  tail(10L)
+
+# Same, but now excluding the 25th bottom percentile in # of ratings
+# This returns a fairly different set (only three movies overlap)
+df %>%
+  group_by(movieId, title) %>%
+  summarize(mean_rating = mean(rating), n_rating = n()) %>%
+  arrange(desc(mean_rating)) %>%
+  filter(n_rating > 30L) %>%
+  tail(10L)
+
+# Plot the relationship between rating count and rating score
+# We find that movies with few ratings have a much wider rating spread
+df %>%
+  group_by(movieId, title) %>%
+  summarize(mean_rating = mean(rating), n_rating = n()) %>%
+  ggplot(mapping = aes(x = n_rating, y = mean_rating)) +
+  geom_point() +
+  theme_light()
+
+# ==============================================================================
+# 2.5 Rating by year of movie release
+# ==============================================================================
+
+# Ratings by year of movie release (table)
+# We find that ratings are more severe for movies released after 1977
+# Median rating is 4.0 for pre-1977 movies, and 3.5 for post-1977 movies
+# Is there some nostalgia at play, whereby older movies are given more leeway?
+# Or are older movies objectively better than more modern ones?df %>%
+  group_by(year_movie) %>%
+  summarize(
+    mean   = mean(rating),
+    median = median(rating),
+    n_rating = n()
+  )
+
+# Ratings by year of movie release (plot)
+df %>%
+  group_by(year_movie) %>%
+  summarize(
+    mean   = mean(rating),
+    median = median(rating),
+    low    = quantile(rating, .25), # 25th percentile of the ratings for that year
+    high   = quantile(rating, .75)  # 75th percentile of the ratings for that year
+  ) %>%
+  ggplot(aes(x = year_movie)) +
+  geom_ribbon(
+    mapping = aes(y = mean, ymin = low, ymax = high),
+    fill    = "grey70"
+  ) +
+  geom_line(mapping = aes(y = mean)) +
+  geom_point(mapping = aes(y = median)) +
+  theme_light() +
+  xlab("Year of movie release") +
+  ylab("Ratings")
+
+# ==============================================================================
+# 2.6 Rating by year of movie release
+# ==============================================================================
+
+# Ratings by year of rating (table)
+# We find that more recent reviews (> 2022) are more severe than older reviews
+df %>%
+  group_by(year_rating) %>%
+  summarize(
+    mean   = mean(rating),
+    median = median(rating),
+    n_rating = n()
+  )
+
+# Ratings by year of rating (plot)
+df %>%
+  group_by(year_rating) %>%
+  summarize(
+    mean   = mean(rating),
+    median = median(rating),
+    low    = quantile(rating, .25), # 25th percentile of the ratings for that year
+    high   = quantile(rating, .75)  # 75th percentile of the ratings for that year
+  ) %>%
+  ggplot(aes(x = year_rating)) +
+  geom_ribbon(
+    mapping = aes(y = mean, ymin = low, ymax = high),
+    fill    = "grey70"
+  ) +
+  geom_line(mapping = aes(y = mean)) +
+  geom_point(mapping = aes(y = median)) +
+  theme_light() +
+  xlab("Year of rating") +
+  ylab("Ratings")
+
+# ==============================================================================
+# 2.7 Rating by years of distance between movie release and rating
+# ==============================================================================
+
+# Ratings by years of distance between movie release and rating (table)
+# Movies reviewed <19 or >88 years after their release are judged more severely
+df %>%
+  group_by(year_dist) %>%
+  summarize(
+    mean   = mean(rating),
+    median = median(rating),
+    n_rating = n()
+  )
+
+# Ratings by years of distance between movie release and rating (plot)
+df %>%
+  group_by(year_dist) %>%
+  summarize(
+    mean   = mean(rating),
+    median = median(rating),
+    low    = quantile(rating, .25), # 25th percentile of the ratings for that year
+    high   = quantile(rating, .75)  # 75th percentile of the ratings for that year
+  ) %>%
+  ggplot(aes(x = year_dist)) +
+  geom_ribbon(
+    mapping = aes(y = mean, ymin = low, ymax = high),
+    fill    = "grey70"
+  ) +
+  geom_line(mapping = aes(y = mean)) +
+  geom_point(mapping = aes(y = median)) +
+  theme_light() +
+  xlab("Years of distance between movie release and rating") +
+  ylab("Ratings")
+
+# ==============================================================================
+# 2.8 Rating by genre
+# ==============================================================================
 
 # Ratings by genre
+# Film-noir is the highest-rated (mean = 3.9), horror the worst-rated (2.85)
+# Is it a bias in reviewer's preferences, or are many horror movies just bad?
+df %>%
+  distinct(title, .keep_all = TRUE) %>%
+  separate_rows(genres, sep = "\\|") %>%
+  group_by(genres) %>%
+  summarize(
+    mean   = mean(rating),
+    median = median(rating),
+    n_rating = n()
+  ) %>%
+  arrange(desc(mean))
+
+# ==============================================================================
+# 3 Predictions
+# ==============================================================================
 
 # ==============================================================================
 # X Housekeeping
